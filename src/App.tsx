10745329ef239466b2713e78539cdf2d5c79b49d
edurodrigues0 <edusacramento9@gmail.com>
2022-09-01 21:02:43 -0300

    finalizando projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,118 @@
 import './global.css';
 
+import { ChangeEvent, FormEvent, useEffect, useState } from 'react';
 import { Check, PlusCircle, Trash } from 'phosphor-react';
 
 import { Header } from './components/Header';
 import { Tasks } from './components/Tasks';
 import styles from './App.module.css';
 
-function App() {
+interface taskListProps {
+  title: string;
+  completed: boolean;
+}
+
+const taskList = [
+  {
+    title: 'Estudar React JS',
+    completed: false,
+  },
+];
+
+export function App() {
+  const [taskText, setTaskText] = useState('');
+  const [newTaskList, setNewTaskList] = useState<taskListProps[]>(taskList)
+  const [completedTaskLength, setCompletedTaskLength] = useState(0);
+
+  function handleNewTaskChange(event: ChangeEvent<HTMLInputElement>): void {
+    setTaskText(event.target.value);
+  }
+
+  function handleAddTaskToTodoList(event: FormEvent) {
+    event.preventDefault();
+    setNewTaskList([...newTaskList, {
+      title: taskText,
+      completed: false,
+    }])
+    setTaskText('');
+  }
+
+  function deleteTask(taskToDelete: string, isCompleted: boolean) {
+    if(isCompleted === false) {
+      return alert('Complete a tarefa para excluir')
+    }
+    const tasksWithoutDeletedOne = newTaskList.filter(task => {
+      return task.title !== taskToDelete;
+    })
+    const completedLength = tasksWithoutDeletedOne.filter(task => {
+      return task.completed === true
+    })
+    setNewTaskList(tasksWithoutDeletedOne);
+    setCompletedTaskLength(completedLength.length);
+  }
+
+  function completedTask(taskTitleCompleted: string, isCompleted: boolean) {
+    newTaskList.filter((task, index) => {
+      if(task.title === taskTitleCompleted) {
+        task.completed = isCompleted;
+        return newTaskList.indexOf(task, index)
+      }
+    })
+    setNewTaskList(newTaskList);
+    const completedLength = newTaskList.filter(task => {
+      return task.completed === true;
+    })
+    setCompletedTaskLength(completedLength.length);
+  }
+
+  const isNewTaskEmpty = taskText.length === 0;
+  
   return(
     <>
       <Header />
       <div className={styles.container}>
-        <form className={styles.addTodoWrapper}>
-          <input placeholder="Adicione uma nova tarefa" type="text" />
-          <button type="submit">Criar <PlusCircle size={16}/></button>
+        <form className={styles.addTodoWrapper} onSubmit={handleAddTaskToTodoList}>
+          <input
+            type="text"
+            placeholder="Adicione uma nova tarefa"
+            value={taskText}
+            onChange={handleNewTaskChange}
+            required
+          />
+          <button 
+            type="submit"
+            disabled={isNewTaskEmpty}
+          >
+            Criar <PlusCircle size={16}/>
+          </button>
         </form>
         <main className={styles.tasks}>
           <div className={styles.tasksInfo}>
             <p>
               Tarefas criadas
-              <span>5</span>
+              <span>{newTaskList.length}</span>
             </p>
             
             <p>
               Concluídas
               <span>
-                2 de 5
+                {completedTaskLength} de {newTaskList.length}
               </span>
             </p>
           </div>
-          <Tasks />
-          <Tasks />
-          <Tasks />
-          <Tasks />
-          <Tasks />
-          
+          {newTaskList.map((task) => {
+            return(
+              <Tasks 
+                key={task.title} 
+                title={task.title}
+                completed={task.completed}
+                onDeleteTask={deleteTask}
+                onCompleteTask={completedTask}
+              />
+            )
+          })}
         </main>
       </div>
     </>
   )
 }
-
-export default App
--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -1,21 +1,49 @@
 import { Check, Trash } from 'phosphor-react';
+import { useEffect, useState } from 'react';
 
 import styles from './Tasks.module.css';
 
-export function Tasks() {
+interface TasksProps {
+  title: string;
+  completed: boolean;
+  onDeleteTask: (task: string, completed: boolean) => void;
+  onCompleteTask: (task: string, isCompleted: boolean) => void;
+}
+
+export function Tasks({ 
+  title, 
+  completed, 
+  onDeleteTask,
+   onCompleteTask, 
+}: TasksProps) {
+  const [isCompleted, setIsCompleted] = useState(completed);
+  
+  useEffect(() => {
+    onCompleteTask(title, isCompleted);
+  }, [isCompleted])
+
+  function handleDeleteTask() {
+    onDeleteTask(title, isCompleted);
+  }
+
+  function handleCompletedTask() {
+    setIsCompleted((state) => {
+      return !state
+    })
+  }
+  
   return (
     <div className={styles.tasksList}>
       <label className={styles.checkbox}>
-        <input type="checkbox" />
+        <input type="checkbox" defaultChecked={isCompleted} onClick={handleCompletedTask} />
         <span className={styles.checkboxPersonalized}>
           <Check />
         </span>
         <p className={styles.taskTitle}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          {title}
         </p>
       </label>
-      <button className={styles.deleteTask}>
+      <button className={styles.deleteTask} onClick={handleDeleteTask}>
         <Trash />
       </button>
     </div>
